2679fe884e6b99cfc31cef6a914e56703f126a3d
TrashPandaG59 <daniel.martin.gonzalez@alumnojoyfe.iepgroup.es>
2025-05-04 10:45:56 +0200

    segundo primer comit que realmente el primero no lo subi bien y lo hice en casa de mi novia y ahora ella está trabajando y no la puedo decir que lo suba o algo porque no teletrabaja

---
+++ GymWeb-TFG/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+/* Si quieres, puedes poner estilos generales aquí o en main.css */
+</style>
---
+++ GymWeb-TFG/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="home">
+      <h1>Bienvenido al Gimnasio para pitos fuertes 💪</h1>
+      <p>¡Prepárate para transformar tu cuerpo, tu mente y tu semen!</p>
+      <img src="../assets/img/lopez buena.jpg" alt="Gym Image" class="gym-image" />
+    </div>
+  </template>
+  
+  <script setup>
+  import '../assets/ViewStyles/HomeView.css'
+  </script>
+  
